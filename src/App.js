4509e32dbc6bacd26ef1c1099f2790453f92bd02
Oluwaseun <ekunkoya.seun@gmail.com>
2021-01-12 16:30:55 +0100

    front end

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import { Switch, Router  } from "react-router-dom"
+import { Products } from "./page/Product"
+import { Navbar } from "./components/Navbar"
 
-function App() {
+import "./App.css"
+
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar />
+      <Switch>
+          <Router exact path='/' component={Products} />
+      </Switch>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
